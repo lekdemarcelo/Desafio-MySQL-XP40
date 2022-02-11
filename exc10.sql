8cf675f1c7ae528642da6ddd2df3060acc36b8f0
samuellgc <samuellgc@hotmail.com>
2022-02-11 00:33:42 -0300

    Ajuste da logica para acerto do desafio a questao 10

--- exc10.sql
+++ exc10.sql
@@ -1,4 +1,4 @@
-SELECT produto.preco, departamento.nome
+SELECT sum(produto.preco * produto.estoque) AS 'Estoque Total', departamento.nome AS Departamentos
 FROM produto
 INNER JOIN departamento ON produto.departamento_codigo = departamento.codigo
 GROUP BY departamento.nome;
